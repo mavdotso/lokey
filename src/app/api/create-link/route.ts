4818a0f264d3ffce75c672e8a2b7cd679b175582
mav <0xmav.eth@gmail.com>
2024-08-31 23:18:20 +0400

    fix env

--- src/app/api/create-link/route.ts
+++ src/app/api/create-link/route.ts
@@ -20,7 +20,7 @@ export async function POST(req: Request) {
             expiresAt,
         });
 
-        const BASE_URL = process.env.NODE_ENV === 'production' ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}` : 'http://localhost:3000';
+        const BASE_URL = process.env.NODE_ENV === 'production' ? `https://${process.env.NEXT_PUBLIC_URL}` : 'http://localhost:3000';
 
         return new Response(
             JSON.stringify({
