33c356afbb075aa5e14c1e7cfd5ad7a753271bdd
mav <0xmav.eth@gmail.com>
2024-09-01 03:17:12 +0400

    fix url /

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,9 +6,8 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function getURL() {
-    let url = process?.env?.NEXT_PUBLIC_SITE_URL ?? 'http://localhost:3000/';
+    let url = process?.env?.NEXT_PUBLIC_SITE_URL ?? 'http://localhost:3000';
 
     url = url.includes('http') ? url : `https://${url}`;
-    url = url.charAt(url.length - 1) === '/' ? url : `${url}/`;
     return url;
 }
