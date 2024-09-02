38a9b40ab9f560fd9abeabd1c657768dbc8335be
mav <0xmav.eth@gmail.com>
2024-09-02 22:01:36 +0400

    working emails

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -20,16 +20,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     providers: [
         Resend({
             apiKey: process.env.AUTH_RESEND_KEY,
-            server: {
-                host: process.env.EMAIL_SERVER_HOST,
-                port: process.env.EMAIL_SERVER_PORT,
-                auth: {
-                    user: process.env.EMAIL_SERVER_USER,
-                    pass: process.env.EMAIL_SERVER_PASSWORD,
-                },
-            },
-            from: process.env.EMAIL_FROM,
-            async sendVerificationRequest({ identifier: email, url, provider: { server, from } }) {
+            async sendVerificationRequest({ identifier: email, url }) {
                 const { host } = new URL(url);
 
                 const { data, error } = await resend.emails.send({
