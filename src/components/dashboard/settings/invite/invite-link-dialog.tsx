ff13ddba7e7a79d71daa16f652ed92ef419f67dc
mav <0xmav.eth@gmail.com>
2024-09-14 10:17:02 +0400

    default invite links

--- src/components/dashboard/settings/invite/invite-link-dialog.tsx
+++ src/components/dashboard/settings/invite/invite-link-dialog.tsx
@@ -1,7 +1,6 @@
 import { Button } from "@/components/ui/button";
-import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { CopyIcon, LinkIcon, RefreshCwIcon } from "lucide-react";
 import { useState, useEffect } from "react";
 import { useMutation, useQuery } from "convex/react";
